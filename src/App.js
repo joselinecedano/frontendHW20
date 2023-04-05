bebadc944b3e4a9a2c5dbf849a00876d0b990ffd
joselinecedano <118120312+joselinecedano@users.noreply.github.com>
2023-04-04 23:13:03 -0400

    set up routes in app.js, imported components and pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+//import components
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+//import router
+import {Route, Routes} from 'react-router-dom';
+
+//import pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
 
 function App() {
+  //link to backend 
+  const URL = 'http://localhost:4000/';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/* import Header */}
+    <Header />
+    {/* add patchs to routes */}
+    <Routes>
+      <Route exact path='/' element={<Home/>} />
+      {/* pass the URL as a prop to about and projects so they can make a call to our API */}
+      <Route exact path='/projects' element={<Projects URL={URL}/>} />
+      <Route exact path='/about' element={<About URL={URL}/>} />
+    </Routes>
+    {/* import footer */}
+    <Footer />
     </div>
   );
 }
