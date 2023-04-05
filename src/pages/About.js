2a63b53262fb2fc39b0921059fdd0a486d35c344
joselinecedano <118120312+joselinecedano@users.noreply.github.com>
2023-04-05 00:57:34 -0400

    about displaying data from backend

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,34 @@
+import { useState, useEffect } from "react";
 const About = (props) => {
-    return (
-      <div>
-        <h1> About Component</h1>
-      </div>
-    );
+  //create state for about data
+  const [about, setAbout] = useState(null);
+
+  //create a function to make api call
+  const getAboutData = async () => {
+    //make api call and get response
+    const response = await fetch(props.URL + "about");
+    //turn resonse into javascript object
+    const data = await response.json();
+    //set the about state to the data
+    setAbout(data);
+    console.log(data);
   };
-  
-  export default About;
-  
+
+  //make an initial call for the data inside of a useEffect, so it only happens ONCE on component load.
+  useEffect(() => {
+    getAboutData();
+  }, []);
+
+  //function that'll return the info needed once we get the the data
+  const loaded = () => (
+    <div>
+      <h2>{about.name}</h2>
+      <h3>{about.email}</h3>
+      <p>{about.bio}</p>
+    </div>
+  );
+  //if data arrives return result of loaded() else return "Loading..."
+  return about ? loaded() : <h1> Loading... </h1>;
+};
+
+export default About;
